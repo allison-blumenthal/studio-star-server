8be95d41afe6f169a3fc935f0049a5384a9de9e8
Allison Blumenthal <blumen.allison@gmail.com>
2023-08-24 21:48:05 -0400

    removed teacher_student model, viewset, and all references, migrated

--- studiostarapi/models/__init__.py
+++ studiostarapi/models/__init__.py
@@ -1,5 +1,4 @@
 from .user import User
-from .teacher_student import TeacherStudent
 from .assignment import Assignment
 from .task import Task
 from .sticker import Sticker
--- studiostarapi/views/__init__.py
+++ studiostarapi/views/__init__.py
@@ -1,6 +1,5 @@
 from .register import check_user, register_user
 from .user import UserView
-from .teacher_student import TeacherStudentView
 from .assignment import AssignmentView
 from .task import TaskView
 from .sticker import StickerView
